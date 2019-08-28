4e2b22beab396e91c2fae9c1f811e6b7e1e0b0ea
dario.zubaray <dzubaray@imagosur.com>
2019-08-28 09:46:26 -0300

    checkeando que la aplicacion no se encuentre levantada tomando un puerto de la maquina local. Abstrayendo logica del main a metodos estativos perifericos

--- src/main/java/com/imagosur/tv/Main.java
+++ src/main/java/com/imagosur/tv/Main.java
@@ -2,6 +2,9 @@ package com.imagosur.tv;
 
 import java.awt.EventQueue;
 import java.awt.im.InputContext;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.SocketException;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -11,29 +14,43 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        InputContext context = InputContext.getInstance();
-        System.out.println("ContextLocale: " + context.getLocale().toString());
+        checkApplicationRunning(100);
+        checkContextLocale();
+        loadsLookAndFeel();
 
         int heigth = getHeigthParam(args);
         int width = getWidthParam(args);
 
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-            System.out.println("Unable to set look and feel");
-        }
-        
         EventQueue.invokeLater(() -> {
             new KeyboardUI(heigth, width).setVisible(true);
         });
     }
 
+    public static void checkApplicationRunning(int port) {
+        try {
+            ServerSocket ss = new ServerSocket();
+            ss.bind(new InetSocketAddress(port));
+            System.out.println("Aplicacion iniciada, ocupando el puerto local: " + port);
+        } catch (SocketException e) {
+            System.out.println("La aplicacion ya se encuentra corriendo, finalizando aplicacion");
+            System.exit(1);
+        } catch (Exception e) {
+            System.out.println("La aplicacion encontro algun problema de arranque: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static void checkContextLocale() {
+        InputContext context = InputContext.getInstance();
+        System.out.println("ContextLocale: " + context.getLocale().toString());
+    }
+
     public static int getHeigthParam(String[] args) {
-        if(args != null && args.length > 0) {
+        if (args != null && args.length > 0) {
             String primerParametro = args[0];
             try {
                 return Integer.parseInt(primerParametro);
-            } catch(NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 return 0;
             }
         }
@@ -41,14 +58,24 @@ public class Main {
     }
 
     public static int getWidthParam(String[] args) {
-        if(args != null && args.length > 1) {
+        if (args != null && args.length > 1) {
             String segundoParametro = args[1];
             try {
                 return Integer.parseInt(segundoParametro);
-            } catch(NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 return 0;
             }
         }
         return 0;
+    }
+
+    public static void loadsLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException ex) {
+            System.out.println("No se pudo cargar el look and feel");
+        }
+
     }
 }
